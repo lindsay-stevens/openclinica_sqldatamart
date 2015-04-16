f86ce1d3f802202cd4827ad646d9e3f995541717
lindsay-stevens-kirby <lstevens@kirby.unsw.edu.au>
2015-04-16 16:53:21 +1000

    update create study schemas; use metadata_study instead of distinct on metadata, only create schemas not already existing

--- scripts/study/dm_create_study_schemas.sql
+++ scripts/study/dm_create_study_schemas.sql
@@ -1,6 +1,5 @@
 CREATE OR REPLACE FUNCTION openclinica_fdw.dm_create_study_schemas(
-  filter_study_name TEXT DEFAULT $$$$,
-  create_or_drop    TEXT DEFAULT $$create$$
+  filter_study_name TEXT DEFAULT $$$$
 )
   RETURNS TEXT AS
   $BODY$
@@ -10,29 +9,31 @@ CREATE OR REPLACE FUNCTION openclinica_fdw.dm_create_study_schemas(
         FOR r IN
         SELECT
             format(
-                    $$CREATE SCHEMA %1$I AUTHORIZATION dm_admin; 
+                    $$CREATE SCHEMA %1$I; 
                     CREATE MATERIALIZED VIEW %1$I.timestamp_schema AS 
                     SELECT %1$L::text AS study_name, now() AS timestamp_schema;
                     CREATE MATERIALIZED VIEW %1$I.timestamp_data AS 
                     SELECT %1$L::text AS study_name, now() as timestamp_data;$$,
-                    sub.study_name
+                    sub.study_name_clean
             ) AS create_statement,
-            format(
-                    $$DROP SCHEMA %1$I CASCADE;$$,
-                    sub.study_name
-            ) AS drop_statement,
-            sub.study_name
+            sub.study_name_clean
         FROM
             (
                 SELECT
-                    DISTINCT ON (metadata.study_name)
-                    dm_clean_name_string(
-                            metadata.study_name
-                    ) AS study_name
+                    dmms.study_name_clean
                 FROM
-                    dm.metadata
+                    dm.metadata_study AS dmms
                 WHERE
-                    metadata.study_name ~ (
+                    dmms.study_status != $$removed$$
+                    AND NOT EXISTS(
+                        SELECT
+                            n.nspname
+                        FROM
+                            pg_namespace AS n
+                        WHERE
+                            n.nspname = dmms.study_name_clean
+                    )
+                    AND dmms.study_name ~ (
                         CASE
                         WHEN length(
                                      filter_study_name
@@ -42,11 +43,7 @@ CREATE OR REPLACE FUNCTION openclinica_fdw.dm_create_study_schemas(
                     )
             ) AS sub
         LOOP
-            IF create_or_drop = $$create$$ THEN
-                EXECUTE r.create_statement;
-            ELSIF create_or_drop = $$drop$$ THEN
-                EXECUTE r.drop_statement;
-            END IF;
+            EXECUTE r.create_statement;
         END LOOP;
         RETURN $$done$$;
     END
