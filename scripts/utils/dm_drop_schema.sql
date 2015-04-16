19db443c6f73cc1c69d1f41cc4418acc342cd351
lindsay-stevens-kirby <lstevens@kirby.unsw.edu.au>
2015-04-16 16:53:54 +1000

    add dm_drop_schema as this option was removed from dm_create_study_schemas

---
+++ scripts/utils/dm_drop_schema.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION openclinica_fdw.dm_drop_schema(
+  schema_name TEXT DEFAULT $$$$
+)
+  RETURNS VOID AS
+  $BODY$
+    BEGIN
+        EXECUTE format($$DROP SCHEMA IF EXISTS %1$I CASCADE $$, dm_clean_name_string(schema_name));
+    END
+    $BODY$
+LANGUAGE plpgsql VOLATILE;
