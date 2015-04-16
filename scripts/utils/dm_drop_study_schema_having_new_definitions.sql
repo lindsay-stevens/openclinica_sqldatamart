f26163fe229d09b3d0340815eb24b2453742dc34
lindsay-stevens-kirby <lstevens@kirby.unsw.edu.au>
2015-04-16 16:55:42 +1000

    update dm_drop_study_schema_having_new_def to use metadata_study and metadata_event_crf_ig

--- scripts/utils/dm_drop_study_schema_having_new_definitions.sql
+++ scripts/utils/dm_drop_study_schema_having_new_definitions.sql
@@ -11,21 +11,20 @@ CREATE OR REPLACE FUNCTION openclinica_fdw.dm_drop_study_schema_having_new_defin
                     DROP TABLE IF EXISTS schema_to_drop;
                     CREATE TEMP TABLE schema_to_drop AS 
                     SELECT DISTINCT ON (dmm.study_name) dmm.study_name
-                    FROM %1$I.metadata AS dmm, %1$I.timestamp_schema AS ts
+                    FROM %1$I.metadata_event_crf_ig AS dmm, %1$I.timestamp_schema AS ts
                     WHERE crf_version_date_created > date_trunc($$day$$, ts.timestamp_schema)
                     OR event_date_created > date_trunc($$day$$, ts.timestamp_schema)
                     OR event_date_updated > date_trunc($$day$$, ts.timestamp_schema)
                     $query$,
-                    dm_clean_name_string(
-                            study_name)
+                    study_name_clean
             ) AS statements, 
             $query$
-            SELECT dm_create_study_schemas(study_name, $$drop$$)
+            SELECT dm_drop_schema(study_name)
             FROM schema_to_drop;
             $query$ AS drop_statement
         FROM
-            dm.metadata
-        WHERE dm_clean_name_string(study_name) IN 
+            dm.metadata_study
+        WHERE study_name_clean IN
             (SELECT nspname FROM pg_catalog.pg_namespace)
         LOOP
             EXECUTE r.statements;
