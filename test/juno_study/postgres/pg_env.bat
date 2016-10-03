4153f7d3e2b2aabf0ccba8a348a8dfc22c8a3202
lindsay stevens <lindsay.stevens.au@gmail.com>
2016-10-03 22:06:38 +1100

    add: files for setup from scratch

---
+++ test/juno_study/postgres/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET "PATH=C:\Program Files\PostgreSQL\9.6\bin;%PATH%"
+@SET PGDATA=C:\Users\Lstevens\Documents\repos\openclinica\openclinica_sqldatamart\test\juno_study\postgres\data
+@SET PGDATABASE=postgres
+@SET PGUSER=postgres
+@SET PGPORT=5446
+@SET PGLOCALEDIR=C:\Program Files\PostgreSQL\9.6\share\locale
+
+                          
